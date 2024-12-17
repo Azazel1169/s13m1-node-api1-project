73d88dbcb2e875d6378286184fe59b5bf91f850e
Josh Rime <joshrime1990@icloud.com>
2024-12-16 23:38:15 -0600

    receiving users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,30 @@
-// BUILD YOUR SERVER HERE
+const express = require('express')
+const server  = express()
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+
+
+server.get('/api/users', (req,res) => {
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The users information could not be retrieved",
+            err: err.message
+        })
+    })
+})
+
+
+
+
+server.use('*', (req,res) => {
+    res.status(404).json({
+        message: "Not Found"
+    })
+})
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
