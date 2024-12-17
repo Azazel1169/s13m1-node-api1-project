eaa5b3a490858430134c2187a0d698c8a9a328ad
Josh Rime <joshrime1990@icloud.com>
2024-12-16 23:58:47 -0600

    getting user by id

--- api/server.js
+++ api/server.js
@@ -1,30 +1,43 @@
-const express = require('express')
-const server  = express()
-const User = require('./users/model')
+const express = require("express");
+const server = express();
+const User = require("./users/model");
 
-
-
-server.get('/api/users', (req,res) => {
-    User.find()
-    .then(users => {
-        res.json(users)
+server.get("/api/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      res.json(users);
     })
-    .catch(err => {
-        res.status(500).json({
-            message: "The users information could not be retrieved",
-            err: err.message
+    .catch((err) => {
+      res.status(500).json({
+        message: "The users information could not be retrieved",
+        err: err.message,
+      });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({
+            message: "The user with the specified ID does not exist",
         })
+      }
+      res.json(user)
     })
-})
+    .catch((err) => {
+      res.status(500).json({
+        message: "The user information could not be retrieved",
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+});
 
-
-
-
-server.use('*', (req,res) => {
-    res.status(404).json({
-        message: "Not Found"
-    })
-})
-
+server.use("*", (req, res) => {
+  res.status(404).json({
+    message: "Not Found",
+  });
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
