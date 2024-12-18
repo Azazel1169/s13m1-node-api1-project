31a3509a27050e6601b2a655f5cffc055f04b168
Josh Rime <joshrime1990@icloud.com>
2024-12-17 22:37:13 -0600

    server.post passing

--- api/server.js
+++ api/server.js
@@ -1,6 +1,28 @@
 const express = require("express");
-const server = express();
 const User = require("./users/model");
+
+const server = express();
+server.use(express.json());
+
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  if (!user.name || !user.bio) {
+    res.status(400).json({
+      message: "Please provide name and bio for the user",
+    });
+  } else {
+    User.insert(user)
+      .then((createdUser) => {
+        res.status(201).json(createdUser);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "There was an error while saving the user to the database",
+          err: err.message,
+        });
+      });
+  }
+});
 
 server.get("/api/users", (req, res) => {
   User.find()
@@ -20,10 +42,10 @@ server.get("/api/users/:id", (req, res) => {
     .then((user) => {
       if (!user) {
         res.status(404).json({
-            message: "The user with the specified ID does not exist",
-        })
+          message: "The user with the specified ID does not exist",
+        });
       }
-      res.json(user)
+      res.json(user);
     })
     .catch((err) => {
       res.status(500).json({
