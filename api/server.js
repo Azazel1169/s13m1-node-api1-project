200e1f7b64f967e40fe6cf914d93d304a09453ef
Josh Rime <joshrime1990@icloud.com>
2024-12-18 00:06:42 -0600

    added error catch in delete request

--- api/server.js
+++ api/server.js
@@ -57,16 +57,29 @@ server.get("/api/users/:id", (req, res) => {
 });
 
 server.delete("/api/users/:id", async (req, res) => {
-  const userId = await User.findById(req.params.id);
-  if (!userId) {
-    res.status(404).json({
-      message: "The user with the specified ID does not exist",
-    });
-  } else {
-    const deletedUser = await User.remove(userId.id);
-    res.status(200).json(deletedUser);
-  }
+ try {
+    const userId = await User.findById(req.params.id);
+    if (!userId) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    } else {
+      const deletedUser = await User.remove(userId.id);
+      res.status(200).json(deletedUser);
+    }
+ } catch (err) {
+    res.status(500).json({
+        message: "The user information could not be retrieved",
+        err: err.message,
+        stack: err.stack,
+      });
+ }
 });
+
+server.put("/api/users/:id", async (req, res) => {
+
+})
+
 
 server.use("*", (req, res) => {
   res.status(404).json({
